28614388404d308280cc2b299e35be4a9e8bb2e1
Till <asuro@posteo.de>
2019-04-20 03:09:58 +0200

    bump version to 1.0.3

--- scorsaber.user.js
+++ scorsaber.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ScoreSaberEnhanced
 // @namespace    https://scoresaber.com
-// @version      1.0.2
+// @version      1.0.3
 // @description  Adds links to beatsaver and add player comparison
 // @author       Splamy, TheAsuro
 // @match        http*://scoresaber.com/*
